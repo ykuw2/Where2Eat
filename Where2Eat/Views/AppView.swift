513a2f36457c2f1ff3e94778d359476dc6f581a3
Yuki <yuki-kuwahara@hotmail.com>
2025-10-31 01:04:10 -0700

    Basic UI Logic Implemented

--- Where2Eat/Views/AppView.swift
+++ Where2Eat/Views/AppView.swift
@@ -9,23 +9,36 @@ import SwiftUI
 
 struct AppView: View {
     @State private var showIntro: Bool = true
-    
+    @AppStorage("userName") private var userName = ""
+
     var body: some View {
-        if showIntro {
-            IntroView()
-                .onAppear {
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
-                        withAnimation(.easeOut(duration: 0.5)) {
-                            showIntro = false
+            if showIntro {
+                IntroView()
+                    .onAppear {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                            withAnimation(.easeOut(duration: 0.5)) {
+                                showIntro = false
+                            }
                         }
                     }
+            } else {
+                ZStack { // We are using ZStack here so that when NameView -> InputView we can have a transition
+                    if userName.isEmpty {
+                        NameView()
+                            .transition(.opacity)
+                    } else {
+                        InputView()
+                            .transition(.opacity)
+                    }
                 }
-        } else {
-            InputView()
+                .animation(.easeOut(duration: 0.5), value: userName)
+            }
         }
     }
-}
+
 
 #Preview {
-    AppView()
+    // Use bottom for testing
+//    UserDefaults.standard.removeObject(forKey: "userName")
+    return AppView()
 }
--- Where2Eat/Views/InputView.swift
+++ Where2Eat/Views/InputView.swift
@@ -8,8 +8,36 @@
 import SwiftUI
 
 struct InputView: View {
+    @AppStorage("userName") private var userName: String = ""
+    @State private var cuisineOrFood: String = ""
+    @State private var location: String = ""
+    @State private var radius: Int = 0
+    
     var body: some View {
-        
+        VStack {
+            Text("Hello \(userName), what would you like to eat today?")
+            
+            Text("Cuisine or Food")
+            TextField("Enter your cuisine of food you're craving", text: $cuisineOrFood)
+                .textFieldStyle(.roundedBorder)
+                .keyboardType(.default)
+                .padding()
+            
+            Text("Location")
+            TextField("Enter your address, city, or a zip code", text: $location)
+                .textFieldStyle(.roundedBorder)
+                .keyboardType(.default)
+                .padding()
+            
+            // TODO: This will later be changed to a wheel of 1, 5, 10, 20... so on
+            Text("Radius")
+            TextField("Enter the radius in which you are willing to go for!", value: $radius, formatter: NumberFormatter())
+                .textFieldStyle(.roundedBorder)
+                .keyboardType(.decimalPad)
+                .padding()
+            
+            Button("Spin The wheel!") {}
+        }
     }
 }
 
