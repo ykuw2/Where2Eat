b986d2ae7cf12e3fb679b7c0426817a80dae9400
Yuki <yuki-kuwahara@hotmail.com>
2025-11-01 17:36:07 -0700

    If pressed go then go to LoadingView

--- Where2Eat/Views/InputView.swift
+++ Where2Eat/Views/InputView.swift
@@ -12,40 +12,52 @@ struct InputView: View {
     @State private var cuisineOrFood: String = ""
     @State private var location: String = ""
     @State private var radius: Int = 1
+    @State private var isLoading = false
     
     let radiusDistance: [Int] = [1, 5, 10, 20, 25, 50, 100]
     
     var body: some View {
-        VStack {
-            Text("Hello \(userName), what would you like to eat today?")
-            
-            Spacer()
-                .frame(height: 100)
-            
-            Text("Cuisine or Food")
-            TextField("Enter your cuisine of food you're craving", text: $cuisineOrFood)
-                .textFieldStyle(.roundedBorder)
-                .keyboardType(.default)
-                .padding()
-            
-            Text("Location")
-            TextField("Enter your address, city, or a zip code", text: $location)
-                .textFieldStyle(.roundedBorder)
-                .keyboardType(.default)
-                .padding()
-            
-            Text("Radius")
-            Picker("Radius", selection: $radius) {
-                ForEach(radiusDistance, id: \.self) { distance in
-                    Text("Within \(distance) \(distance == 1 ? "mile" : "miles")")
+        ZStack {
+            if !isLoading {
+                VStack {
+                    Text("Hello \(userName), what would you like to eat today?")
+                    
+                    Spacer()
+                        .frame(height: 100)
+                    
+                    // TODO: Make sure that the two fields below are not empty
+                    Text("Cuisine or Food")
+                    TextField("Enter your cuisine of food you're craving", text: $cuisineOrFood)
+                        .textFieldStyle(.roundedBorder)
+                        .keyboardType(.default)
+                        .padding()
+                    
+                    Text("Location")
+                    TextField("Enter your address, city, or a zip code", text: $location)
+                        .textFieldStyle(.roundedBorder)
+                        .keyboardType(.default)
+                        .padding()
+                    
+                    Text("Radius")
+                    Picker("Radius", selection: $radius) {
+                        ForEach(radiusDistance, id: \.self) { distance in
+                            Text("Within \(distance) \(distance == 1 ? "mile" : "miles")")
+                        }
+                    }
+                    .pickerStyle(.menu)
+                    
+                    Spacer()
+                        .frame(height: 100)
+                    
+                    // TODO: Add logic for the MapKit
+                    // For now it will just go to the LoadingView
+                    Button("Go!") {
+                        isLoading = true
+                    }
                 }
+            } else { // if isLoading is true
+                LoadingView()
             }
-            .pickerStyle(.menu)
-            
-            Spacer()
-                .frame(height: 100)
-            
-            Button("Go!") {}
         }
     }
 }
