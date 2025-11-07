acc669ac52c9387df8422cd58a22e775791a4195
Yuki <yuki-kuwahara@hotmail.com>
2025-11-07 03:07:50 -0800

    Initial version for MapKit request but still trying to understand the framework and code lol

--- Where2Eat/Models/MapData.swift
+++ Where2Eat/Models/MapData.swift
@@ -2,5 +2,32 @@
 //  MapData.swift
 //  Where2Eat
 //
-//  Created by Yuki Kuwahara on 11/1/25.
+//  Created by Yuki Kuwahara on 11/6/25.
 //
+
+import MapKit
+
+func getRestaurants(foodOrCuisine: String, location: String, distance: Int, completion: @escaping ([MKMapItem]) -> Void) {
+    let locationRequest = MKLocalSearch.Request()
+    
+    locationRequest.naturalLanguageQuery = location
+    locationRequest.resultTypes = .address // Only get the .address
+    
+    MKLocalSearch(request: locationRequest).start { response, error in
+        if let error = error {
+            print("Location search error: \(error.localizedDescription)")
+            completion([])
+            return
+        }
+        
+        guard let response = response else {
+            print("No results found.")
+            completion([])
+            return
+        }
+        
+        let items: [MKMapItem] = response.mapItems
+        completion(items)
+        
+    }
+}
